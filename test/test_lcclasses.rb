2fac65cb036f3acc811ae67445fae1f784fb31b9
William Melody <hi@williammelody.com>
2010-02-25 19:28:58 -0600

    renaming test file for autotest

--- test/test_lcclasses.rb
+++ test/test_lcclasses.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class LcclassesTest < Test::Unit::TestCase
+class TestLCClasses < Test::Unit::TestCase
   should "probably rename this file and start testing for real" do
     flunk "hey buddy, you should probably rename this file and start testing for real"
   end
